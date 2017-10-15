db7f3e5b664a3b7b27cbb3f1e8f9f8706be60dcb
Julian Sanchez <Wathnai@hotmail.com>
2017-10-16 00:15:57 +0200

    programmingAssigment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,61 @@
 ## functions do
 
 ## Write a short comment describing this function
+##The first function, makeVector creates a special "vector", which is really a list containing a function to
+##set the value of the vector
+##get the value of the vector
+##set the value of the mean
+##get the value of the mean
+
+
+## The function makecachematrix creates a matrix,
+## later we have to populate "m" with the function "m$set" i.e -m$set(matrix(c(1:4),2,2))
+## Additionaly we can get the value of "m" "using m$get()"
+## alternatively we can get or set the inverse of "m", getinv will get the value of "m" assigned in setinv.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) m <<- inverse
+    getinv <- function() m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## the function cachesolve takes the inverse of "m", if "m" is not null,
+## will get the "cache data" message and the value.
+## If not it will call the get and setinv functions within makecachematrix 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+cacheSolve <- function(x){
+     m <- x$getinv()
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     data <- x$get()
+     m <- solve(data)
+     x$setinv(m)
+     m
+ }
+##test
+## > source("cachematrix.R")
+# > m <- makeCacheMatrix()
+# > m$set(matrix(c(1:4),2,2))
+# > m$get()
+# [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+# > cacheSolve(m)
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > cacheSolve(m)
+# getting cached data
+# [,1] [,2]
+# [1,]   -2  1.5
+# [2,]    1 -0.5
+# > 
